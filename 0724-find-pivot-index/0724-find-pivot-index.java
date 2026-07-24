dc2b86a5eec9c5882d4553fede60e6aec3d2ac0d
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-07-25 00:00:42 +0530

    Time: 0 ms (100%), Space: 47.6 MB (22.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -2,8 +2,8 @@ class Solution {
     public int pivotIndex(int[] nums) {
 
         int totalSum = 0;
-        for(int num : nums) {
-            totalSum += num;
+        for(int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
         }
 
         int leftSum = 0;
