9256b3e641efd2f83df638e4a9018e5b7a490cfc
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-07-28 23:57:43 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.64%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,25 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+
         
-        for(int i = digits.length - 1; i>=0; i--){
-            if(digits[i] != 9){
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            // If the current digit is less than 9,
+            // simply increment it and return.
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
+
+            // If the digit is 9, it becomes 0
+            // and the carry moves to the next digit.
             digits[i] = 0;
         }
-        int[] arr1 = new int[digits.length + 1];
-        arr1[0] = 1;
-        return arr1;
+
+        // If all digits were 9, create a new array
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
     }
 }
