8101c87b48f2155d2a3a76e3d26bf9e048977fdb
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-07-30 17:47:39 +0530

    Time: 9 ms (43.39%), Space: 42 MB (70.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int fib(int n) {
-        return fibonacci(n);
         
-    }
-
-    public int fibonacci(int n) {
+        
         if(n <= 1) return n;
 
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return fib(n - 1) + fib(n - 2);
     }
 }
